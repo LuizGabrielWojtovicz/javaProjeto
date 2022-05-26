673fa061ffe71afdce375814ccbe551e8bc23b0a
LuizGabrielWojtovicz <83958914+LuizGabrielWojtovicz@users.noreply.github.com>
2022-05-26 11:04:39 -0300

    Create Table.java

---
+++ Table.java
@@ -0,0 +1,36 @@
+public static void lineTableInter(String tm, String bord)
+	{
+		for(int i = 0; i < (tm.length() + 3); i++)
+		{
+			if(i == 0)
+			{
+				System.out.format("%s", bord.charAt(0));
+			}
+			else if(i > 0)
+			{
+				System.out.format("─");
+			}
+			if(i == (tm.length() + 2))
+			{
+				System.out.format("%s",bord.charAt(1));
+			}
+		}
+		
+	}
+	
+	public static void table(String txt)
+	{
+		String bordSup = "╭╮";
+		String bordSub = "╰╯";
+		
+		lineTableInter(txt, bordSup);
+		
+		System.out.println();
+		
+		System.out.format("│ %s │", txt);
+		
+		System.out.println();
+		
+		lineTableInter(txt, bordSub);
+		
+	}
